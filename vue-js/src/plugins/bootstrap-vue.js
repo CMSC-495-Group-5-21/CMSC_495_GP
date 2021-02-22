a9fb645423d83b4a628d71ae26b831887fe6c60b
Naomi Bonnin <naomi172839@gmail.com>
2021-02-22 12:14:45 -0500

    Deletion of old files

--- vue-js/src/plugins/bootstrap-vue.js
+++
@@ -1,7 +0,0 @@
-import vue from "vue";
-
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-Vue.use(BootstrapVue);
