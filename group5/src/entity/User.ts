0428c573413f320d764311d667f64c6112129cef
Naomi Bonnin <naomi172839@gmail.com>
2021-02-23 14:58:37 -0500

    Created Models, Created initial migration,
    Signed-off-by: Naomi Bonnin <naomi172839@gmail.com>

--- group5/src/entity/User.ts
+++ group5/src/entity/User.ts
@@ -1,10 +1,19 @@
-import {Entity,Column,PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn,DeleteDateColumn} from "typeorm";
+import {Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  OneToMany
+} from "typeorm";
+
+import {Reservation} from "./Reservation"
 
 @Entity()
 export class User {
 
   @PrimaryGeneratedColumn("uuid")
-  uuid!: number;
+  uuid!: string;
 
   @Column({length: 40, unique: true})
   username!: string;
@@ -26,6 +35,12 @@ export class User {
 
   @Column({nullable: true})
   prefix!: string;
+
+  @OneToMany(() => Reservation, reservation => reservation.createdBy)
+  reservations!: Reservation[];
+
+  @Column({default: false})
+  admin!: boolean;
 
   @CreateDateColumn()
   created!: Date;
