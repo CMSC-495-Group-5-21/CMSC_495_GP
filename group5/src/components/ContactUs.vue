984468982686b950668c45460d24efb88491a86e
vanderpunk <69319211+vanderpunk@users.noreply.github.com>
2021-03-04 22:06:44 -0500

    Modified ContactUs, HelloWorld, ProfileCreation, and Reservation

--- group5/src/components/ContactUs.vue
+++ group5/src/components/ContactUs.vue
@@ -1,43 +1,54 @@
 <template>
-    <div>
-        <h1> Please enter your information</h1>
-        <form id="contactus" method="POST">
-            <!-- name text -->
-            <div class="name">
-                <p>Name</p>
-                <input type="text" v-model="msg">
-            </div>
-            <!-- Message text -->
-            <div class="textbox">
-                <p>Message</p>
-                <input type="text" v-model="email">
-            </div>
-            <div class="container-fluid">
-                <button class="btn btn btn-danger">
-                    <router-link to="thankyou">
-                        Submit
-                    </router-link>
-                </button>
-            </div>
-        </form>
-    </div>
+  <div>
+    <h1>Please enter your information</h1>
+    <form id="contactus" method="POST">
+      <!-- name text -->
+      <label for="contactUsName" class="grey-text"> Name </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        class="form-control"
+        placeholder="ex Rolan Shmidtt"
+        v-model="name"
+      />
+      <br />
+      <br />
+      <!-- Message text -->
+      <label for="contactUsMessage" class="contactUs"> Message : </label>
+      <textarea
+        id="contactUsMessage"
+        name="contactUsMessage"
+        class="form-control"
+        placeholder="Please enter message"
+        v-model="specialRequests"
+        rows="1"
+        columns="50"
+      />
+      <br />
+      <br />
+      <!-- Submit Button -->
+      <div class="container-fluid">
+        <button class="btn btn btn-danger">
+          <router-link to="thankyou"> Submit </router-link>
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang='ts'>
-  export default{
-    setup() {
-      const msg = 'name'
-    }
-  }
+export default {
+  setup() {
+    const msg = "name";
+  },
+};
 </script>
 
 <style>
-.textbox{
+.textbox {
+}
 
-  }
-
-.name{
-
-  }
-
+.name {
+}
 </style>
--- group5/src/components/Reservation.vue
+++ group5/src/components/Reservation.vue
@@ -1,170 +1,218 @@
 <template>
-    <div>
-        <h1>Reserve a Room at Rosebudd Hotel</h1>
-        <form id="newReservation" class="justify-content-center" @submit.prevent="this.processForm">
-            <!-- login -->
-            <div class="name">
-                <label for="user" class="grey-text">Reservation For: </label>
-                <br>
-                <label v-if="!!this.uuid" id='user' name='user'>{{this.firstName + ' ' + this.lastName}}</label>
-                <button v-else class="btn btn btn-danger">
-                    <router-link to="login"> Login </router-link>
-                </button>
-            </div>
-            <!-- date text -->
-            <div class="date">
-                <label for="startDate" class="grey-text"> Start Date: </label>
-                <br>
-                <input type="date" id='startDate' name='startDate' class='form-control' v-model="startDate"/>
-                <br>
-                <label for="endDate" class="grey-text"> End Date: </label>
-                <br>
-                <input type="date" id='endDate' name='endDate' class='form-control' v-model="endDate"/>
-            </div>
-            <!-- Room select -->
-            <div v-show="this.show">
-                <label v-if="!this.openRooms.length" id="notfound" name="notfound">
-                    No Open Rooms Found
-                </label>
-                <table v-else id="openRooms" name="openRooms" class="table mt-5">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Room Type</th>
-                            <th scope="col">Room Cost</th>
-                            <th scope="col">Select</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(room, i) in this.openRooms" :key="i">
-                            <th scope="row">{{++i}}</th>
-                            <td>{{room.room_uuid}}</td>
-                            <td>{{this.roomTypes.get(room.room_roomTypeUuid)}}</td>
-                            <td>{{this.roomCosts.get(room.room_roomTypeUuid)}}</td>
-                            <td><input type="radio" name="assignedRoom" id="assignedRoom" v-model="assignedRoom" :value="room.room_uuid"/></td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-            <br>
-            <button class="btn btn btn-danger" id="getrooms" @click.prevent="this.getRooms">Get Open Rooms</button>
-
-            <!-- Special Requests -->
-            <br />
-            <label for="specialRequests" class="requests">
-                Special Requests:
-            </label>
-            <br />
-            <textarea id="specialRequest" name="specialRequest" class="form-control" placeholder="Please enter requests (Optional)" v-model="specialRequests" rows="6" columns="50" />
-            <br />
-            <br />
-            <div class="container-fluid">
-                <button class="btn btn btn-danger" type="submit">Submit
-                </button>
-                <button class="btn btn btn-standard">
-                    <router-link to="/"> Home </router-link>
-                </button>
-            </div>
-        </form>
-    </div>
+  <div>
+    <h1>Reserve a Room at Rosebudd Hotel</h1>
+    <form
+      id="newReservation"
+      class="justify-content-center"
+      @submit.prevent="this.processForm"
+    >
+      <!-- login -->
+      <div class="name">
+        <label for="user" class="grey-text">Reservation For: </label>
+        <br />
+        <label v-if="!!this.uuid" id="user" name="user">{{
+          this.firstName + " " + this.lastName
+        }}</label>
+      </div>
+      <!-- date text -->
+      <div class="date">
+        <br />
+        <label for="startDate" class="grey-text"> Start Date: </label>
+        <input
+          type="date"
+          id="startDate"
+          name="startDate"
+          class="form-control"
+          v-model="startDate"
+        />
+        <br />
+        <br />
+        <label for="endDate" class="grey-text"> End Date: </label>
+        <input
+          type="date"
+          id="endDate"
+          name="endDate"
+          class="form-control"
+          v-model="endDate"
+        />
+      </div>
+      <!-- Room select -->
+      <div v-show="this.show">
+        <label v-if="!this.openRooms.length" id="notfound" name="notfound">
+          No Open Rooms Found
+        </label>
+        <table v-else id="openRooms" name="openRooms" class="table mt-5">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">ID</th>
+              <th scope="col">Room Type</th>
+              <th scope="col">Room Cost</th>
+              <th scope="col">Select</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(room, i) in this.openRooms" :key="i">
+              <th scope="row">{{ ++i }}</th>
+              <td>{{ room.room_uuid }}</td>
+              <td>{{ this.roomTypes.get(room.room_roomTypeUuid) }}</td>
+              <td>{{ this.roomCosts.get(room.room_roomTypeUuid) }}</td>
+              <td>
+                <input
+                  type="radio"
+                  name="assignedRoom"
+                  id="assignedRoom"
+                  v-model="assignedRoom"
+                  :value="room.room_uuid"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <button
+        class="btn btn btn-danger"
+        id="getrooms"
+        @click.prevent="this.getRooms"
+      >
+        Get Open Rooms
+      </button>
+      <!-- Special Requests -->
+      <br />
+      <br />
+      <label for="specialRequests" class="requests"> Special Requests: </label>
+      <textarea
+        id="specialRequest"
+        name="specialRequest"
+        class="form-control"
+        placeholder="Please enter requests (Optional)"
+        v-model="specialRequests"
+        rows="6"
+        columns="50"
+      />
+      <br />
+      <br />
+      <!-- Submit and Home Buttons -->
+      <ul>
+        <li>
+          <div class="container-fluid">
+            <button class="btn btn btn-standard" type="submit">
+              <router-link to="thankyou"> Submit </router-link>
+            </button>
+          </div>
+        </li>
+        <li>
+          <div class="container-fluid">
+            <button class="btn btn btn-standard">
+              <router-link to="/"> Home </router-link>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import axios from 'axios';
+import axios from "axios";
 axios.defaults.withCredentials = true;
-import router from '../router';
+import router from "../router";
 export default {
-    data() {
-        let uuid = '';
-        let firstName = '';
-        let lastName = '';
-        let startDate = '';
-        let endDate = '';
-        let assignedRoom = '';
-        let specialRequests = '';
-        let openRooms = [];
-        let show = false;
-        let roomTypes = new Map();
-        let roomCosts = new Map();
+  data() {
+    let uuid = "";
+    let firstName = "";
+    let lastName = "";
+    let startDate = "";
+    let endDate = "";
+    let assignedRoom = "";
+    let specialRequests = "";
+    let openRooms = [];
+    let show = false;
+    let roomTypes = new Map();
+    let roomCosts = new Map();
     return {
-        uuid,
-        firstName,
-        lastName,
-        startDate,
-        endDate,
-        assignedRoom,
-        specialRequests,
-        openRooms,
-        show,
-        roomTypes,
-        roomCosts
+      uuid,
+      firstName,
+      lastName,
+      startDate,
+      endDate,
+      assignedRoom,
+      specialRequests,
+      openRooms,
+      show,
+      roomTypes,
+      roomCosts,
     };
   },
   methods: {
-      processForm: function() {
-          let data = new FormData();
-          data.append('startDate', this.startDate);
-          data.append('endDate', this.endDate);
-          data.append('assignedRoom', this.assignedRoom);
-          data.append('specialRequests', this.specialRequests);
-          data.append('createdBy', this.uuid);
-          data.append('adminCreate', false);
-          axios.post('http://localhost:4000/newReservation', data)
-          .then(response => this.response = response.data)
-          .then(router.push('thankyou'))
-          .then(router.go(1))
-          .catch(error => {
-              alert(error);
+    processForm: function () {
+      let data = new FormData();
+      data.append("startDate", this.startDate);
+      data.append("endDate", this.endDate);
+      data.append("assignedRoom", this.assignedRoom);
+      data.append("specialRequests", this.specialRequests);
+      data.append("createdBy", this.uuid);
+      data.append("adminCreate", false);
+      axios
+        .post("http://localhost:4000/newReservation", data)
+        .then((response) => (this.response = response.data))
+        .then(router.push("thankyou"))
+        .then(router.go(1))
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    getRooms: function () {
+      this.show = true;
+      let data = new FormData();
+      data.append("startDate", this.startDate);
+      data.append("endDate", this.endDate);
+      axios
+        .post("http://localhost:4000/getOpenRooms", data)
+        .then((response) => response.data)
+        .then((data) => (this.openRooms = data))
+        .catch((error) => {
+          alert(error);
+        });
+      console.log(this.openRooms);
+    },
+    getTypes: function () {
+      axios
+        .get("http://localhost:4000/getAllRoomTypes")
+        .then((response) => {
+          let roomTypes = new Map();
+          let roomCosts = new Map();
+          response.data.forEach((room) => {
+            this.roomTypes.set(room.uuid, room.typeName);
+            this.roomCosts.set(room.uuid, room.typeCost);
           });
-
-      },
-      getRooms: function() {
-          this.show=true;
-          let data = new FormData();
-          data.append('startDate', this.startDate);
-          data.append('endDate', this.endDate);
-          axios.post('http://localhost:4000/getOpenRooms', data)
-              .then(response => response.data)
-              .then(data => this.openRooms = data)
-              .catch(error => {
-                  alert(error);
-              });
-              console.log(this.openRooms);
-      },
-      getTypes: function() {
-          axios.get('http://localhost:4000/getAllRoomTypes')
-          .then((response) => {
-              let roomTypes = new Map();
-              let roomCosts = new Map();
-              response.data.forEach( (room) => {
-                  this.roomTypes.set(room.uuid, room.typeName);
-                  this.roomCosts.set(room.uuid, room.typeCost);
-              });
-          }).catch(error => {
-              alert(error);
-          });
-      },
-      getCookie: function(name) {
-          let matches = document.cookie.match(new RegExp(
-              "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-          ));
-          return matches ? decodeURIComponent(matches[1]) : undefined;
-      }
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    getCookie: function (name) {
+      let matches = document.cookie.match(
+        new RegExp(
+          "(?:^|; )" +
+            name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+            "=([^;]*)"
+        )
+      );
+      return matches ? decodeURIComponent(matches[1]) : undefined;
+    },
   },
-  update() {
-  },
+  update() {},
   mounted() {
-      let uuid = this.getCookie("uuid");
-      this.uuid = uuid;
-      let firstName = this.getCookie("firstName");
-      this.firstName = firstName;
-      let lastName = this.getCookie("lastName");
-      this.lastName = lastName;
-      this.getTypes();
-
-  }
+    let uuid = this.getCookie("uuid");
+    this.uuid = uuid;
+    let firstName = this.getCookie("firstName");
+    this.firstName = firstName;
+    let lastName = this.getCookie("lastName");
+    this.lastName = lastName;
+    this.getTypes();
+  },
 };
 </script>
 
@@ -177,5 +225,10 @@ export default {
 
 .container-fluid {
   color: black;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
 }
 </style>
